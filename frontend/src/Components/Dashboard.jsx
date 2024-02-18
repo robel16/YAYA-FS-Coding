5900b68e6b1f30b296e5a573165187dc062c78d0
robel16 <robelmuluwork@gmail.com>
2024-02-18 03:16:04 +0300

    minor changes

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -59,7 +59,7 @@ function Dashboard() {
       createdAt: "2024-02-12 10:00:00",
     },
   ];
-
+  //** comment out the transaction to use the dummy data */
   const dataToShow = transaction || dummyData;
   return (
     <>
@@ -427,19 +427,6 @@ function Dashboard() {
               </div>
               <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
                 <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-pink-600 to-pink-400 text-white shadow-pink-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
-                  {/* <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    aria-hidden="true"
-                    className="w-6 h-6 text-white"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg> */}
                   <div className="w-30 h-39 text-white">
                     <img src={Image1} alt="" />
                   </div>
